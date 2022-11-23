99376ddb73fee52ac690b60ade43f524b4fb2d12
KorotkovAndrey2022 <psoffice.korotkov@yandex.ru>
2022-11-23 22:42:33 +0300

    изменения

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 using namespace std;
 
+int Sum(int a, int b);               // Прототип функции (Оьявление функции - Function declaration)
+int Differenve(int a, int b);
+int Product(int a, int b);
+int Quotiht(int a, int b);
 
 void main()
 {
 	setlocale(LC_ALL, "Rus");
-	cout << "Hello Functions" << endl;
 
+	int a, b;
+
+	cout << "Введите два чила: "; cin >> a >> b;
+
+	int c = Sum(a, b); // Вызов функции - Function call
+	// Too few arguments in function call
+	// Too many arguments in function call  
+
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Differenve(a, b) << endl;
+	cout << a << " * " << b << " = " << Product(a, b) << endl;
+	cout << a << " / " << b << " = " << Quotiht(a, b) << endl;
 }
+
+int Sum( int a, int b ) // Реализация функции (Опредение функции - Funсtion definition)
+{
+	int sum = a + b;
+	return sum;
+}
+
+int Differenve(int a, int b)
+
+{
+	return a - b;
+}
+int Product(int a,int b)
+{
+	return a *b;
+}
+
+int Quotiht(int a, int b)
+{
+	return (double) a / b;
+}
+
